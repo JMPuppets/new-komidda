a12d22b85a6fe2ee09f4e33c484515e9db56d606
JMPuppets <37351112+JMPuppets@users.noreply.github.com>
2019-06-05 19:00:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,20 @@ client.on('message', message => {
   	}
 });
 
+client.on("message", (message) =>{
+    if (message.content.startsWith("komaeda, ") {
+        let responses = ["No fucking chance.", 
+        "You're fucking joking right? Why on Earth would you ask that? Do you have a gosh darn motherfucking deathwish, you motherfucking motherfucker? Never ask me something that disrespectful again.",
+        "No.", 
+        "Probably.", 
+        "Yes! Absolutely.",
+        "No fucking chance.",
+        "Perhaps.",
+        "https://cdn.discordapp.com/attachments/559515079264174097/559891374091010077/perhaps.jpg"];
+        let pick = responses[Math.floor(Math.random()*array.length)];
+        message.content.send(pick);
+    }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
